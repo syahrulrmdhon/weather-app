bc56de099f211cb631774852256d92c732287f2c
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2019-01-07 17:38:18 +0700

    Updates

---
+++ precache-manifest.3845d965fa7c480437e5277aec64edfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "b90bd440241bf9bb405b",
+    "url": "/weather-app/static/js/main.b90bd440.chunk.js"
+  },
+  {
+    "revision": "3c6d536c5d998dc74e20",
+    "url": "/weather-app/static/js/1.3c6d536c.chunk.js"
+  },
+  {
+    "revision": "b90bd440241bf9bb405b",
+    "url": "/weather-app/static/css/main.05b1a9c9.chunk.css"
+  },
+  {
+    "revision": "251d4728583cb63d576a808c936f6079",
+    "url": "/weather-app/index.html"
+  }
+];
